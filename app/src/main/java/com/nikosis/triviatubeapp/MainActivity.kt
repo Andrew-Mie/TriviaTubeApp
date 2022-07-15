f9ba0f633bf30fa511591bcb2724370776bb50ff
Andy <andy4tunes@gmail.com>
2022-07-15 01:20:19 -0700

    Fixed Main

--- app/src/main/java/com/nikosis/triviatubeapp/MainActivity.kt
+++ app/src/main/java/com/nikosis/triviatubeapp/MainActivity.kt
@@ -11,7 +11,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.nikosis.triviatubeapp.ui.theme.TriviaTubeAppTheme
+import com.nikosis.triviatubeapp.ui.view.screen.TriviaHomeScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,17 +25,49 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
+//                    MainScreen()
+                    TriviaHomeScreen()
                 }
             }
         }
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TriviaTubeAppTheme {
+//@Composable
+//fun MainScreen() {
+//    Box(modifier = Modifier.padding(2.dp)) {
+//        Button(onClick = { /*TODO*/ }) {
+//
+//        }
+//    }
+//}
 
-    }
-}
+//@Composable
+//fun TriviaScreen() {
+//    val viewModel: TriviaViewModel = viewModel()
+//    val rememberedTrivia : MutableState<List<SecondResponse>> = remember {
+//        mutableStateOf(emptyList<SecondResponse>())
+//    }
+//    viewModel.getTrivia() { response ->
+//        val triviaFromApi = response?.serverDto
+//        rememberedTrivia.value = triviaFromApi.orEmpty()
+//    }
+//    Text(text = rememberedTrivia.value.toString())
+//}
+//
+//
+//@Composable
+//fun Cardoa() {
+//    Card(modifier = Modifier.padding(4.dp)) {
+//        Text(text = "Hi".repeat(150), maxLines = 3,overflow = TextOverflow.Ellipsis)
+//    }
+//}
+//
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    TriviaTubeAppTheme {
+//        Cardoa()
+//    }
+//}
