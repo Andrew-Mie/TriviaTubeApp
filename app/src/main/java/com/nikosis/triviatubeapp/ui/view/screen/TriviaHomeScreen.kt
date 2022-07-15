dacff0b0972672fe4826769fbf9a2778f5601146
Andy <andy4tunes@gmail.com>
2022-07-15 03:20:37 -0700

    Added TopBar
    TopBar Implementation into home screen

--- app/src/main/java/com/nikosis/triviatubeapp/ui/view/screen/TriviaHomeScreen.kt
+++ app/src/main/java/com/nikosis/triviatubeapp/ui/view/screen/TriviaHomeScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -16,39 +17,42 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.nikosis.triviatubeapp.ui.view.viewmodel.TriviaViewModel
+import com.nikosis.triviatubeapp.ui.view.widgets.TriviaAppTopBar
 import com.nikosis.triviatubeapp.ui.view.widgets.TriviaItemCard
 
 @Composable
 fun TriviaHomeScreen(viewModel: TriviaViewModel = hiltViewModel()) {
     val state = viewModel.state.value
-    Box(modifier = Modifier.fillMaxSize()) {
-        state.triviaLoaded?.let {
-            LazyColumn {
-                items(state.triviaLoaded.drmItems.subList(fromIndex = 0, toIndex = 10)) { x ->
-                    TriviaItemCard(
-                        title = x.snippet.title,
-                        desc = x.snippet.description,
-                        picUrl = x.snippet.thumbnails.default.url
-                    )
+    Scaffold(topBar = { TriviaAppTopBar() }) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            state.triviaLoaded?.let {
+                LazyColumn {
+                    items(state.triviaLoaded.drmItems.subList(fromIndex = 0, toIndex = 10)) { x ->
+                        TriviaItemCard(
+                            title = x.snippet.title,
+                            desc = x.snippet.description,
+                            picUrl = x.snippet.thumbnails.default.url
+                        )
+                    }
                 }
             }
-        }
 
-        if (state.loadingError.isNotBlank()) {
+            if (state.loadingError.isNotBlank()) {
 
-            Text(
-                text = state.loadingError,
-                color = MaterialTheme.colors.error,
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 20.dp)
-                    .align(Alignment.Center)
-            )
-        }
+                Text(
+                    text = state.loadingError,
+                    color = MaterialTheme.colors.error,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 20.dp)
+                        .align(Alignment.Center)
+                )
+            }
 
-        if (state.isLoading) {
-            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+            if (state.isLoading) {
+                CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+            }
         }
     }
 }
